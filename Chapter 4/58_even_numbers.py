930f08436efc19e266e06e928390de547c7132a8
Trudie Marques <your-github-email@example.com>
2026-06-09 13:22:17 +0200

    chapter4

---
+++ Chapter 4/58_even_numbers.py
@@ -0,0 +1,8 @@
+# even_numbers = list(range(2, 11, 2))
+# print(even_numbers)
+
+# even_numbers = list(range(0, 25, 3))
+# print(even_numbers)
+
+even_numbers = list(range(0, 105, 5))
+print(even_numbers)
---
+++ Chapter 4/66_dimensions.py
@@ -0,0 +1,21 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+# dimensions[0] = 250
+# print(dimensions)
+
+my_t = (3,)
+print(my_t)
+
+for dimension in dimensions :
+    print(dimension)
+
+print("\nOriginal dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
